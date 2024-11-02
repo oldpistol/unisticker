f44e8a32fbf3d74001c6a9f644044dec98670f20
Ismail bin Aman <oldpistol@gmail.com>
2024-11-02 23:40:05 +0800

    ignore eslint during build

---
+++ frontend/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // ... other Next.js config options
+}
+
+module.exports = nextConfig 
