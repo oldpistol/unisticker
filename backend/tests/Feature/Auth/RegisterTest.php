270d559be5f67418fec54012713ca30e908f1bca
Ismail Aman <oldpistol@gmail.com>
2024-11-15 01:36:44 +0800

    test: add registration tests for valid and invalid user inputs

--- backend/tests/Feature/Auth/RegisterTest.php
+++ backend/tests/Feature/Auth/RegisterTest.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Response;
 
 uses(RefreshDatabase::class);
 
+// test valid register
 it('user can register using ic no', function () {
 
     $user = User::factory()->make();
@@ -38,6 +39,7 @@ it('user can register using ic no', function () {
     ]);
 });
 
+// test valid register
 it('user can register using passport no', function () {
 
     $user = User::factory()->make();
@@ -68,4 +70,27 @@ it('user can register using passport no', function () {
         'matric_id' => $user->matric_id,
         'email' => $user->email,
     ]);
+});
+
+// test invalid register
+it('user cannot register using existing email', function () {
+
+    $user = User::factory()->create();
+
+    $response = $this->post('/api/auth/register', [
+        'name' => $user->name,
+        'matric_id' => $user->matric_id,
+        'phone_no' => $user->phone_no,
+        'ic_no' => $user->ic_no,
+        'passport_no' => null,
+        'matric_id' => $user->matric_id,
+        'email' => $user->email,
+        'password' => 'password',
+        'password_confirmation' => 'password',
+    ]);
+
+    $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+        ->assertJsonValidationErrors([
+            'email' => 'The email has already been taken.'
+        ]);
 });
